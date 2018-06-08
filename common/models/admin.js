d00656595fcf38142ec54ef66cb29e8f42073540
Naveenphp <talla049@gmail.com>
2018-06-08 12:14:33 +0530

    files upload

---
+++ common/models/admin.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function (Admin) {
+	delete Admin.validations.email;
+};
---
+++ server/boot/storage.js
@@ -0,0 +1,18 @@
+
+
+var shortid = require('shortid');
+
+module.exports = function(app) {
+   //Function for checking the file type..
+   app.dataSources.storage.connector.getFilename = function(file, req, res) {
+
+       var pattern = /^image\/.+$/;
+       var value = pattern.test(file.type);
+       if(value ){
+           return shortid.generate() + '.' + file.name.split('.').pop();
+       }
+       else{
+           // throw "FileTypeError: Only File of Image type is accepted.";
+       }
+   };
+}
